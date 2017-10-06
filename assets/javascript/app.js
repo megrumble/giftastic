480791d9c7907c6b8d71c59eb73ef2057ea81e05
Margaret Rumble <megrumble@gmail.com>
2017-10-06 15:17:16 -0400

    not-working animation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,10 @@
 // initial array of foods
 var topics = ["watermelon", "pumpkin", "tomato", "pie", "cake", "pizza", "spaghetti", ];
 //make butttons
+var src;
+var dataUrl;
+var foodImage;
+
 function renderButtons() {
     $("#food-view").empty();
     for (i = 0; i < topics.length; i++) {
@@ -27,58 +31,56 @@ function clearInput() {
     $("#food-input").focus();
 }
 $(".food").on("click", function (event) {
-// api_key = bOKLbAqau8gyELiXlbxmxG4gKbfXELE4;
-var food = $(this).attr("data-name");
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + food + "&api_key=bOKLbAqau8gyELiXlbxmxG4gKbfXELE4&limit=10";
-//create ajax call
-$.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-    // After data comes back from the request
-    .done(function (response) {
-        console.log(queryURL);
+    // api_key = bOKLbAqau8gyELiXlbxmxG4gKbfXELE4;
+    var food = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + food + "&api_key=bOKLbAqau8gyELiXlbxmxG4gKbfXELE4&limit=10";
+    //create ajax call
+    $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        // After data comes back from the request
+        .done(function (response) {
+            console.log(queryURL);
 
-        console.log(response);
-        // storing the data from the AJAX request in the results variable
-        var results = response.data;
-        for (var i = 0; i < results.length; i++) {
+            console.log(response);
+            // storing the data from the AJAX request in the results variable
+            var results = response.data;
+            for (var i = 0; i < results.length; i++) {
 
-            // Creating and storing a div tag
-            var foodDiv = $("<div>");
+                // Creating and storing a div tag
+                var foodDiv = $("<div>");
 
-            // Creating a paragraph tag with the result item's rating
-            var p = $("<p>").text("Rating: " + results[i].rating);
+                // Creating a paragraph tag with the result item's rating
+                var p = $("<p>").text("Rating: " + results[i].rating);
 
-            // Creating and storing an image tag
-            var foodImage = $("<img>");
-            // Setting the src attribute of the image to a property pulled off the result item
-            foodImage.attr("src", results[i].images.fixed_height_still.url);
-            foodImage.addClass("image");
-            // Appending the paragraph and image tag to the foodDiv
-            foodDiv.append(p);
-            foodDiv.append(foodImage)
-            $("#gifs-appear-here").prepend(foodDiv);
-        };
-        $(".image").on("click", function () {
+                // Creating and storing an image tag
+                foodImage = $("<img>");
+                // Setting the src attribute of the image to a property pulled off the result item
+                foodImage.attr({
+                    "src": results[i].images.fixed_height_still.url,
+                    "data-url": results[i].images.fixed_height.url
+                });
+              
+                foodImage.addClass("image");
+                // Appending the paragraph and image tag to the foodDiv
+                foodDiv.append(p);
+                foodDiv.append(foodImage)
+                $("#gifs-appear-here").prepend(foodDiv);
+            };
+            $(".image").on("click", function () {
 
-            console.log("hi")
-            console.log(this);
+                console.log("hi")
+                console.log(this);
+                src = foodImage.attr["src"];
+                dataUrl = foodImage.attr["data-url"];
+                var temp=src;
+                src=dataUrl;
+                dataUrl=temp;
 
-            // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-            var state = $(this).attr("data-state");
-            // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-            // Then, set the image's data-state to animate
-            // Else set src to the data-still value
-            if (state === "still") {
-                $(this).attr("src", $(this).attr("data-animate"));
-                $(this).attr("data-state", "animate");
-            } else {
-                $(this).attr("src", $(this).attr("data-still"));
-                $(this).attr("data-state", "still");
-            }
+
+            });
         });
-    });
 
 
 });
